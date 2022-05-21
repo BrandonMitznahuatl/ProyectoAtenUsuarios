8da9f43d09102c4afdd500d66e46c15e255f7aa7
BrandonJairVillagranaAparicio <201820327@tese.edu.mx>
2022-05-20 21:31:23 -0500

    Modificacio de Micuenta y ResgisterProb

---
+++ src/app/registerprob/registerprob.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registerprob',
+  templateUrl: './registerprob.component.html',
+  styleUrls: ['./registerprob.component.css']
+})
+export class RegisterprobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
