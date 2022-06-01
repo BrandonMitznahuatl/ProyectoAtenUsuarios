98c5f30c16e78cf095d6a1d30caf3b1f8c009574
BrandonJairVillagranaAparicio <201820327@tese.edu.mx>
2022-05-31 20:25:27 -0500

    agregar ruta de micuenta y catalogo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RegisterComponent } from './register/register.component';
 import { ProblemtrackingComponent } from './problemtracking/problemtracking.component';
-
+import { CatalogoComponent } from './catalogo/catalogo.component'
+import { MicuentaComponent } from './micuenta/micuenta.component';
 const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'problemtracking', component: ProblemtrackingComponent}
+  {path: 'problemtracking', component: ProblemtrackingComponent},
+  {path: 'catalogo', component: CatalogoComponent},
+  {path: 'micuenta', component: MicuentaComponent},
 ];
 
 @NgModule({
