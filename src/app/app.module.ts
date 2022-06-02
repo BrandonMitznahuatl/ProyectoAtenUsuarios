a6b34259d963127e26038a6aa756ed4ceb019de1
Alin-Hernandez <201820703@tese.edu.mx>
2022-06-02 10:34:19 -0500

    Revert "cambio letra, tamaño y color"
    This reverts commit b5817c07ac9b068d53dbc6fea051ea727642bea7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser' ;
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,9 +31,7 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule
   ],
-
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
