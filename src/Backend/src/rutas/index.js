7eb3e4801404dc31ea00cbdad62cc7330e7b394e
BrandonReyesMitznahuatl <201822042@tese.edu.mx>
2022-06-22 19:52:20 -0500

    Creación de Indexjs e instalación de nodemon

---
+++ src/Backend/src/rutas/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const port = ( process.env.port || 4200);
+const app = express();
+app.set('port', port);
+app.listen(app.get('port'));
+console.log('Servidor');
+
