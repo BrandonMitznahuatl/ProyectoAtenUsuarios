a0c8c5203062d6323d806ea17e4b44532f44d19c
BrandonJairVillagranaAparicio <201820327@tese.edu.mx>
2022-06-23 00:04:15 -0500

    corregir

--- src/Backend/src/rutas/rutas.js
+++ src/Backend/src/rutas/rutas.js
@@ -8,18 +8,7 @@ routes.get('/',(req,res)=>{
 
 const conexion = require("../config/conexionBD")
 
-routes.get('/bdmicuenta',(req,res)=>{
-    let query = "select *from micuenta";
-    conexion.query(query,(error, rows)=>{
-        if(error){
-            res.send(error);
-        }
-        else{
-            res.send(rows);
-        }
-    })
-    //res.send('Base de datos');
-})
+
 //Ruta seguimeinto del problema
 routes.get('/bdseguimiento',(req,res)=>{
     let query = "SELECT * FROM problemtracking;";
