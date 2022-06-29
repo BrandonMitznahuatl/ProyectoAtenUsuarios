717a740f23c3eea6f1818e49f9f8ad53aa728d72
Daniela Michelle Aviña Velarde <201821785@tese.edu.mx>
2022-06-29 09:53:18 -0500

    actualizacion rutas

--- src/Backend/src/config/conexionBD.js
+++ src/Backend/src/config/conexionBD.js
@@ -2,7 +2,6 @@ const mysql = require("mysql")
 const conecta = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"sasa",
     password:"root",
     port:"3306",
     database:"ProyectAten"
--- src/Backend/src/rutas/rutas.js
+++ src/Backend/src/rutas/rutas.js
@@ -10,8 +10,62 @@ const conexion = require("../config/conexionBD")
 
 
 //Ruta seguimeinto del problema
+routes.get('/bbsegprob',(req,res)=>{
+    let query = "SELECT * FROM problemtracking    ";
+    conexion.query(query,(error, rows)=>{
+        if(error){
+            res.send(error);
+        }
+        else{
+            res.send(rows);
+        }
+    })
+    //res.send('Base de datos');
+})
+//Ruta solucion del problema
 routes.get('/bdsolprob',(req,res)=>{
     let query = "select *from solprob";
+    conexion.query(query,(error, rows)=>{
+        if(error){
+            res.send(error);
+        }
+        else{
+            res.send(rows);
+        }
+    })
+    //res.send('Base de datos');
+})
+-//Ruta de Loc¿ginAcceso
+routes.get('/bdAcceso',(req,res)=>{
+    let query = "SELECT * FROM LoginAcceder;";
+    conexion.query(query,(error, rows)=>{
+        if(error){
+            res.send(error);
+        }
+        else{
+            res.send(rows);
+        }
+    })
+    //res.send('Base de datos');
+})
+
+//Ruta de Loc¿ginRecuperar
+routes.get('/bdRecuperar',(req,res)=>{
+    let query = "SELECT * FROM LoginRecuperar;";
+    conexion.query(query,(error, rows)=>{
+        if(error){
+            res.send(error);
+        }
+        else{
+            res.send(rows);
+        }
+    })
+    //res.send('Base de datos');
+})
+
+//Ruta de LoginRegistrar
+routes.get('/bdRegistrar',(req,res)=>{
+    let query = "SELECT * FROM LoginRegistro;";
     conexion.query(query,(error, rows)=>{
         if(error){
             res.send(error);
