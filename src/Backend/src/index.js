2325b9a9756d49c62019099307d45a0e940ebb8e
Andrea Alin Hernández Pacheco <101274848+Alin-Hernandez@users.noreply.github.com>
2022-06-30 22:33:48 -0500

    Ruta Tabla Rol - POST

--- src/Backend/src/index.js
+++ src/Backend/src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const port = ( process.env.port || 4000);
 const app = express();
+const port = ( process.env.port || 4000);
+app.use(express.json())
 app.set('port', port);
-app.listen(app.get('port'));
+app.listen(app.get('port'),(error)=>{
+    if(error)
+    {
+        console.log('Error al iniciar el servidor '+ error)
+    }else{
+        console.log('Servidor iniciado '+port)
+    }
+})
 console.log('Hola soy servidor');
 
 app.use('/conex',require('./rutas/rutas.js'))
