d6693dfdd4586ac19daffeb57cf6fe55c80158ed
Daniela Michelle Aviña Velarde <201821785@tese.edu.mx>
2022-07-01 17:40:07 -0500

    ORDEN INDEX

--- src/Backend/src/index.js
+++ src/Backend/src/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
 const port = ( process.env.port || 4000);
-
 app.use(express.json())
-
 app.set('port', port);
-
 app.listen(app.get('port'),(error)=>{
     if(error)
     {
@@ -14,6 +11,5 @@ app.listen(app.get('port'),(error)=>{
         console.log('Servidor iniciado en el puerto: '+port)
     }
 })
- 
-app.use('/api',require('./rutas/rutas.js'))
 
+app.use('/api',require('./rutas/rutas.js'))
